3d9d3db98f298336ee7dc62822c82be930d28526
Christiane Dumont <christiane@consultantweb.eu>
2023-08-29 14:46:04 +0200

    Ajout exercice hello js

---
+++ exo1-hello-world/main.js
@@ -0,0 +1,14 @@
+// Affichage d'un message sur la console
+console.log("Hello World");
+window.console.log('Bonjour');
+
+// Affichage dans une fenêtre popup
+window.alert("Hello World");
+// window est facultatif
+alert("Bonjour");
+
+// Afficher des informations sur la page HTML
+document.open();
+document.write("<h2>Hello World</h2>");
+document.write("<p>Et zou.</p>");
+document.close();
